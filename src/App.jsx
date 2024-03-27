a4a3a0986ff982b9219646ae855db75de69ffae5
Lalit kumar rout <routlalit172@gmail.com>
2024-03-27 17:37:47 +0530

    movix

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import Sresult from "./pages/sresult/Sresult";
+
+import Explore from "./pages/explore/Explore";
+import Pagesntfound from "./pages/404/Pagesntfound";
+
+import "./App.css";
+
+function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+  useEffect(() => {
+    fetchApiConfig();
+    genresCall();
+  }, []);
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
+    });
+  };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+
+    dispatch(getGenres(allGenres));
+  };
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<Sresult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<Pagesntfound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/404/Pagesntfound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.scss";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+const Pagesntfound = () => {
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default Pagesntfound;
